135bd773e23017e8b613d57a2e60be21ff5d9d36
ssm8704 <157220041+ssm8704@users.noreply.github.com>
2024-03-03 11:13:23 +0530

    Add files via upload

--- 2048game/script.js
+++ 2048game/script.js
@@ -24,11 +24,15 @@ var num_col_hex = [
     "gray",
     "white"
 ]
-function getpower(ele){
-if(ele!==''){
+function getpower(ele,b){
+    if(ele!==''){
         let p=0
         while(2**(p+1)!==ele)
             p+=1;
+        if(p>=9)
+            document.getElementById(b).style.fontSize="25px";
+        else
+        document.getElementById(b).style.fontSize="35px";
         return p
     }
     else
@@ -241,59 +245,59 @@ function displayMessage(message) {
 function update_panel() {
     // Implement the updatePanel function logic to update the game board display
     document.getElementById("b1").innerHTML=r1[0][0];
-    document.getElementById("b1").style.color=num_col_hex[getpower(r1[0][0])];
-    document.getElementById("b1").style.borderColor=num_col_hex[getpower(r1[0][0])];
+    document.getElementById("b1").style.color=num_col_hex[getpower(r1[0][0],"b1")];
+    document.getElementById("b1").style.borderColor=num_col_hex[getpower(r1[0][0],"b1")];
     document.getElementById("b2").innerHTML=r1[0][1];
-    document.getElementById("b2").style.color=num_col_hex[getpower(r1[0][1])];
-    document.getElementById("b2").style.borderColor=num_col_hex[getpower(r1[0][1])];
+    document.getElementById("b2").style.color=num_col_hex[getpower(r1[0][1],"b2")];
+    document.getElementById("b2").style.borderColor=num_col_hex[getpower(r1[0][1],"b2")];
     document.getElementById("b3").innerHTML=r1[0][2];
-    document.getElementById("b3").style.color=num_col_hex[getpower(r1[0][2])];
-    document.getElementById("b3").style.borderColor=num_col_hex[getpower(r1[0][2])];
+    document.getElementById("b3").style.color=num_col_hex[getpower(r1[0][2],"b3")];
+    document.getElementById("b3").style.borderColor=num_col_hex[getpower(r1[0][2],"b3")];
     document.getElementById("b4").innerHTML=r1[0][3];
-    document.getElementById("b4").style.color=num_col_hex[getpower(r1[0][3])];
-    document.getElementById("b4").style.borderColor=num_col_hex[getpower(r1[0][3])];
+    document.getElementById("b4").style.color=num_col_hex[getpower(r1[0][3],"b4")];
+    document.getElementById("b4").style.borderColor=num_col_hex[getpower(r1[0][3],"b4")];
     document.getElementById("b5").innerHTML=r1[1][0];
-    document.getElementById("b5").style.color=num_col_hex[getpower(r1[1][0])];
-    document.getElementById("b5").style.borderColor=num_col_hex[getpower(r1[1][0])];
+    document.getElementById("b5").style.color=num_col_hex[getpower(r1[1][0],"b5")];
+    document.getElementById("b5").style.borderColor=num_col_hex[getpower(r1[1][0],"b5")];
     document.getElementById("b6").innerHTML=r1[1][1];
-    document.getElementById("b6").style.color=num_col_hex[getpower(r1[1][1])];
-    document.getElementById("b6").style.borderColor=num_col_hex[getpower(r1[1][1])];
+    document.getElementById("b6").style.color=num_col_hex[getpower(r1[1][1],"b6")];
+    document.getElementById("b6").style.borderColor=num_col_hex[getpower(r1[1][1],"b6")];
     document.getElementById("b7").innerHTML=r1[1][2];
-    document.getElementById("b7").style.color=num_col_hex[getpower(r1[1][2])];
-    document.getElementById("b7").style.borderColor=num_col_hex[getpower(r1[1][2])];
+    document.getElementById("b7").style.color=num_col_hex[getpower(r1[1][2],"b7")];
+    document.getElementById("b7").style.borderColor=num_col_hex[getpower(r1[1][2],"b7")];
     document.getElementById("b8").innerHTML=r1[1][3];
-    document.getElementById("b8").style.color=num_col_hex[getpower(r1[1][3])];
-    document.getElementById("b8").style.borderColor=num_col_hex[getpower(r1[1][3])];
+    document.getElementById("b8").style.color=num_col_hex[getpower(r1[1][3],"b8")];
+    document.getElementById("b8").style.borderColor=num_col_hex[getpower(r1[1][3],"b8")];
     document.getElementById("b9").innerHTML=r1[2][0];
-    document.getElementById("b9").style.color=num_col_hex[getpower(r1[2][0])];
-    document.getElementById("b9").style.borderColor=num_col_hex[getpower(r1[2][0])];
+    document.getElementById("b9").style.color=num_col_hex[getpower(r1[2][0],"b9")];
+    document.getElementById("b9").style.borderColor=num_col_hex[getpower(r1[2][0],"b9")];
     document.getElementById("b10").innerHTML=r1[2][1];
-    document.getElementById("b10").style.color=num_col_hex[getpower(r1[2][1])];
-    document.getElementById("b10").style.borderColor=num_col_hex[getpower(r1[2][1])];
+    document.getElementById("b10").style.color=num_col_hex[getpower(r1[2][1],"b10")];
+    document.getElementById("b10").style.borderColor=num_col_hex[getpower(r1[2][1],"b10")];
 
     document.getElementById("b11").innerHTML=r1[2][2];
-    document.getElementById("b11").style.color=num_col_hex[getpower(r1[2][2])];
-    document.getElementById("b11").style.borderColor=num_col_hex[getpower(r1[2][2])];
+    document.getElementById("b11").style.color=num_col_hex[getpower(r1[2][2],"b11")];
+    document.getElementById("b11").style.borderColor=num_col_hex[getpower(r1[2][2],"b11")];
 
     document.getElementById("b12").innerHTML=r1[2][3];
-    document.getElementById("b12").style.color=num_col_hex[getpower(r1[2][3])];
-    document.getElementById("b12").style.borderColor=num_col_hex[getpower(r1[2][3])];
+    document.getElementById("b12").style.color=num_col_hex[getpower(r1[2][3],"b12")];
+    document.getElementById("b12").style.borderColor=num_col_hex[getpower(r1[2][3],"b12")];
 
     document.getElementById("b13").innerHTML=r1[3][0];
-    document.getElementById("b13").style.color=num_col_hex[getpower(r1[3][0])];
-    document.getElementById("b13").style.borderColor=num_col_hex[getpower(r1[3][0])];
+    document.getElementById("b13").style.color=num_col_hex[getpower(r1[3][0],"b13")];
+    document.getElementById("b13").style.borderColor=num_col_hex[getpower(r1[3][0],"b13")];
 
     document.getElementById("b14").innerHTML=r1[3][1];
-    document.getElementById("b14").style.color=num_col_hex[getpower(r1[3][1])];
-    document.getElementById("b14").style.borderColor=num_col_hex[getpower(r1[3][1])];
+    document.getElementById("b14").style.color=num_col_hex[getpower(r1[3][1],"b14")];
+    document.getElementById("b14").style.borderColor=num_col_hex[getpower(r1[3][1],"b14")];
 
     document.getElementById("b15").innerHTML=r1[3][2];
-    document.getElementById("b15").style.color=num_col_hex[getpower(r1[3][2])];
-    document.getElementById("b15").style.borderColor=num_col_hex[getpower(r1[3][2])];
+    document.getElementById("b15").style.color=num_col_hex[getpower(r1[3][2],"b15")];
+    document.getElementById("b15").style.borderColor=num_col_hex[getpower(r1[3][2],"b15")];
 
     document.getElementById("b16").innerHTML=r1[3][3];
-    document.getElementById("b16").style.color=num_col_hex[getpower(r1[3][3])];
-    document.getElementById("b16").style.borderColor=num_col_hex[getpower(r1[3][3])];
+    document.getElementById("b16").style.color=num_col_hex[getpower(r1[3][3],"b16")];
+    document.getElementById("b16").style.borderColor=num_col_hex[getpower(r1[3][3],"b16")];
     if (isWon()) {
         displayMessage("You Won!!!");
     } else if (isOver()) {
